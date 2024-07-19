f8d9589066ddc6386d3c19a752735c762bfcf1c3
sonphamhust <sonstudyhust@gmail.com>
2024-07-19 18:38:14 +0700

    first commit

---
+++ re_id.py
@@ -0,0 +1,176 @@
+import numpy as np
+from torchvision import models, transforms
+import time
+import os
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+from torch.nn import init
+from torch.autograd import Variable
+from torch.nn.utils import fuse_conv_bn_eval
+
+def fuse_all_conv_bn(model):
+    stack = []
+    for name, module in model.named_children():
+        if list(module.named_children()):
+            fuse_all_conv_bn(module)
+            
+        if isinstance(module, nn.BatchNorm2d):
+            if not stack:
+                continue
+            if isinstance(stack[-1][1], nn.Conv2d):
+                setattr(model, stack[-1][0], fuse_conv_bn_eval(stack[-1][1], module))
+                setattr(model, name, nn.Identity())
+        else:
+            stack.append((name, module))
+    return model
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in') # For old pytorch, you may use kaiming_normal.
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
+    elif classname.find('BatchNorm1d') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+    if hasattr(m, 'bias') and m.bias is not None:
+        init.constant_(m.bias.data, 0.0)
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        init.normal_(m.weight.data, std=0.001)
+        init.constant_(m.bias.data, 0.0)
+
+def activate_drop(m):
+    classname = m.__class__.__name__
+    if classname.find('Drop') != -1:
+        m.p = 0.1
+        m.inplace = True
+
+# Defines the new fc layer and classification layer
+# |--Linear--|--bn--|--relu--|--Linear--|
+class ClassBlock(nn.Module):
+    def __init__(self, input_dim, class_num, droprate, relu=False, bnorm=True, linear=512, return_f = False):
+        super(ClassBlock, self).__init__()
+        self.return_f = return_f
+        add_block = []
+        if linear>0:
+            add_block += [nn.Linear(input_dim, linear)]
+        else:
+            linear = input_dim
+        if bnorm:
+            add_block += [nn.BatchNorm1d(linear)]
+        if relu:
+            add_block += [nn.LeakyReLU(0.1)]
+        if droprate>0:
+            add_block += [nn.Dropout(p=droprate)]
+        add_block = nn.Sequential(*add_block)
+        add_block.apply(weights_init_kaiming)
+
+        classifier = []
+        classifier += [nn.Linear(linear, class_num)]
+        classifier = nn.Sequential(*classifier)
+        classifier.apply(weights_init_classifier)
+
+        self.add_block = add_block
+        self.classifier = classifier
+    def forward(self, x):
+        x = self.add_block(x)
+        if self.return_f:
+            f = x
+            x = self.classifier(x)
+            return [x,f]
+        else:
+            x = self.classifier(x)
+            return x
+
+# Define the ResNet50-based Model
+class ft_net(nn.Module):
+
+    def __init__(self, class_num=751, droprate=0.5, stride=2, circle=False, ibn=False, linear_num=512):
+        super(ft_net, self).__init__()
+        model_ft = models.resnet50(pretrained=True)
+        if ibn==True:
+            model_ft = torch.hub.load('XingangPan/IBN-Net', 'resnet50_ibn_a', pretrained=True)
+        # avg pooling to global pooling
+        if stride == 1:
+            model_ft.layer4[0].downsample[0].stride = (1,1)
+            model_ft.layer4[0].conv2.stride = (1,1)
+        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.model = model_ft
+        self.circle = circle
+        self.classifier = ClassBlock(2048, class_num, droprate, linear=linear_num, return_f = circle)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.model.avgpool(x)
+        x = x.view(x.size(0), x.size(1))
+        x = self.classifier(x)
+        return x
+
+data_transforms = transforms.Compose([
+    transforms.Resize((256, 128), interpolation=3),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# Load the model
+def load_network(network, name, which_epoch):
+    save_path = os.path.join('./model', name, 'net_%s.pth' % which_epoch)
+    network.load_state_dict(torch.load(save_path))
+    return network
+
+# Extract feature
+def fliplr(img):
+    '''flip horizontal'''
+    inv_idx = torch.arange(img.size(3)-1, -1, -1).long()  # N x C x H x W
+    img_flip = img.index_select(3, inv_idx)
+    return img_flip
+
+def extract_feature(model, dataloaders, linear_num, batchsize, ms=[1]):
+    pbar = tqdm()
+    for iter, data in enumerate(dataloaders):
+        img, label = data
+        n, c, h, w = img.size()
+        pbar.update(n)
+        ff = torch.FloatTensor(n, linear_num).zero_().cuda()
+        for i in range(2):
+            if i == 1:
+                img = fliplr(img)
+            input_img = Variable(img.cuda())
+            for scale in ms:
+                if scale != 1:
+                    input_img = nn.functional.interpolate(input_img, scale_factor=scale, mode='bicubic', align_corners=False)
+                outputs = model(input_img) 
+                ff += outputs
+        fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+        ff = ff.div(fnorm.expand_as(ff))
+
+        if iter == 0:
+            features = torch.FloatTensor(len(dataloaders.dataset), ff.shape[1])
+        start = iter * batchsize
+        end = min((iter + 1) * batchsize, len(dataloaders.dataset))
+        features[start:end, :] = ff
+    pbar.close()
+    return features
+
+# sort the images
+def sort_img(qf, gf, gl, gc):
+    query = qf.view(-1,1)
+    # print(query.shape)
+    score = torch.mm(gf,query)
+    score = score.squeeze(1).cpu()
+    score = score.numpy()
+    # predict index
+    index = np.argsort(score)  #from small to large
+    index = index[::-1]
+    return index
---
+++ test.py
@@ -0,0 +1,3 @@
+from ultralytics import YOLO
+model2_path = 'weight_yolo/best_custom_02.pt'
+model_detect = YOLO(model2_path)
